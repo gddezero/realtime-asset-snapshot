e1d6a4edabf07df4e8fd571e5c61a011eb1d2f4f
gddezero <gdde_0@hotmail.com>
2021-12-07 01:59:21 +0000

    v0.1

--- data-generator/generator.py
+++ data-generator/generator.py
@@ -3,6 +3,9 @@ import argparse
 import pymysql
 import logging
 import random
+import time
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 class AssetDB():
     def __init__(self, host, user, password, db):
@@ -10,6 +13,7 @@ class AssetDB():
         # self.user = user
         # self.password = password
         # self.db = db
+        print(db_password)
         self.connection = pymysql.connect(host=host, user=user, password=password, database=db)
 
     def execute(self, sql, args=None):
@@ -22,19 +26,51 @@ class AssetDB():
             cursor.executemany(sql, args)
         self.connection.commit()
 
-    def init_db(self, symbols, user_id_range=(1,100000)):
-        for i in range(user_id_range):
-            sql = "insert into user_asset (user_id, asset, balance) values (%s, %s, %s)"
+    def init_db(self, symbols, min_user_id=2, max_user_id=100):
+        for i in range(min_user_id, max_user_id):
+            sql = "insert into user_asset (user_id, token, balance) values (%s, %s, %s)"
             args = []
             for symbol in symbols:
                 arg = (i, symbol, round(random.uniform(1.0, 100.0), 8))
                 args.append(arg)
+                # logging.info(f"Generating user_id: {i}, token: {symbol}")
             self.executemany(sql, args)
+            
+    def upsert(self, symbols, batch_size, max_user_id):
+        sql = "insert into user_asset (user_id, token, balance) values (%s, %s, %s) on duplicate key update balance=values(balance)"
+        while True:
+            args = []
+            for i in range(batch_size):
+                user_id = random.randint(1, max_user_id)
+                token = symbols[random.randint(1, len(symbols) - 1)]
+                balance = round(random.uniform(1.0, 100.0), 8)
+                args.append((user_id, token, balance))
+                logging.info(f"({user_id}, {token}, {balance})")
+            
+            self.executemany(sql, args)
+            logging.info(f"{batch_size} records upserted.")
+            time.sleep(interval)
+            
+    def upsertfew(self, user_ids, tokens):
+        sql = "insert into user_asset (user_id, token, balance) values (%s, %s, %s) on duplicate key update balance=values(balance)"
+        while True:
+            args = []
+            for user_id in user_ids:
+                for token in tokens:
+                    balance = round(random.uniform(1.0, 100.0), 8)
+                    args.append((user_id, token, balance))
+                    logging.info(f"({user_id}, {token}, {balance})")
+
+            self.executemany(sql, args)
+            logging.info(f"{len(user_ids) * len(tokens)} records upserted.")
+            time.sleep(interval)
+                
+        
 
 def get_password(project_id, secret_id):
     client = secretmanager.SecretManagerServiceClient()
     response = client.access_secret_version(request={"name": f"projects/{project_id}/secrets/{secret_id}/versions/latest"})
-    payload = response.payload.data.decode("UTF-8")
+    payload = response.payload.data.decode("UTF-8").strip()
     return payload
 
 def get_symbols():
@@ -50,23 +86,42 @@ if __name__ == "__main__":
     parser.add_argument('--db_user', help='db username')
     parser.add_argument('--db_schema', help='db schema')
     parser.add_argument('--init', action='store_true', help='init db')
+    parser.add_argument('--upsertfew', action='store_true', help='upsert a few users')
+    parser.add_argument('--user_ids', help='user ids to upsert, comma seperated')
+    parser.add_argument('--tokens', help='tokens to upsert, comma seperated')
+    parser.add_argument('--min_user_id', type=int, help='minimum user id')
+    parser.add_argument('--max_user_id', type=int, help='maximum user id')
+    parser.add_argument('--batch_size', type=int, help='batch size of each upsert sql')
+    parser.add_argument('--interval', type=int, help='how many seconds to sleep between each batch')
 
     args = parser.parse_args()
     project_id = args.project
     secret_id = args.secret
     db_host = args.db_host
     db_user = args.db_user
+    db_schema = args.db_schema
+    min_user_id = args.min_user_id
+    max_user_id = args.max_user_id
+    batch_size = args.batch_size
+    user_ids = args.user_ids
+    tokens = args.tokens
+    interval = args.interval
     db_password = get_password(project_id, secret_id)
     symbols = get_symbols()
     
-    print(db_password)
-    print(symbols)
-    # db = AssetDB(db_host, db_user, db_password, db_schema)
-    # db.execute("select 1")
+    db = AssetDB(db_host, db_user, db_password, db_schema)
+    db.execute("select 1")
     
+    logging.info("Start generator")
+
     if args.init:
         logging.info("Initializing DB...")
-        db.init_db(symbols)
+        db.init_db(symbols, min_user_id, max_user_id)
         logging.info("DB initialized. Exit.")
         exit(0)
     
+    if args.upsertfew:
+        db.upsertfew(user_ids.split(","), tokens.split(","))
+        exit(0)
+    
+    db.upsert(symbols, batch_size, max_user_id)
